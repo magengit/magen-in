6ebc68014aa32317aa1eebb2f9bd4818e79a2bfa
Reinaldo Penno <rapenno@gmail.com>
2017-10-21 17:41:55 -0700

    Fixing License

--- ingestion/setup.py
+++ ingestion/setup.py
@@ -61,7 +61,7 @@ setup(
         'Topic :: Security',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: Other/Proprietary License',
+        'License :: Apache',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
